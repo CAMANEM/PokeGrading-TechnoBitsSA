a59f1b82bdd27663c52296b462d4b68ba3efb677
Oscar Soto <62962211+CAMANEM@users.noreply.github.com>
2026-06-26 18:53:51 -0600

    DD new column and relation properly fixed

--- backend/db/init/001_schema.sql
+++ backend/db/init/001_schema.sql
@@ -68,6 +68,7 @@ CREATE TABLE "card_submitter" (
   "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "submitter_id" bigint NOT NULL,
   "hash_id" bigint,
+  "card_reference_id" bigint,
   "img_id" BIGINT GENERATED ALWAYS AS IDENTITY,
   "display_name" varchar,
   "set_name" varchar,
@@ -167,6 +168,8 @@ ALTER TABLE "admin" ADD FOREIGN KEY ("language_id") REFERENCES "language" ("id")
 ALTER TABLE "card_submitter" ADD FOREIGN KEY ("submitter_id") REFERENCES "submitter" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "card_submitter" ADD FOREIGN KEY ("hash_id") REFERENCES "hash_submitter" ("id") DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "card_submitter" ADD FOREIGN KEY ("card_reference_id") REFERENCES "card_reference" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "card_submitter" ADD FOREIGN KEY ("language_id") REFERENCES "language" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
